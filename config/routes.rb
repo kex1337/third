e0901b467fb559045df9eefd1d9382ad6a2278ee
root <sasha6157@gmail.com>
2020-01-13 21:57:35 +0300

    idk what

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'help', to: 'static_pages#help'
   get 'static_pages/about'
   get 'static_pages/contacts'
-  get 'sign', to: 'users#new'
+  get 'signup', to: 'users#new'
 
   resources :contrs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
